046dc7db826f25a7613297e0465408c9ac9223d2
Todd Kao <todd.kakao@gmail.com>
2025-04-13 19:06:47 -0400

    test

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,23 @@
-import withPWA from "next-pwa";
 
-const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+import { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } from "next/constants";
+
+module.exports = async (phase: string) => {
+  /** @type {import("next").NextConfig} */
+  const nextConfig: import("next").NextConfig = {
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+  };
+
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withSerwist = (await import("@serwist/next")).default({
+      // Note: This is only an example. If you use Pages Router,
+      // use something else that works, such as "service-worker/index.ts".
+      swSrc: "app/sw.ts",
+      swDest: "public/sw.js",
+    });
+    return withSerwist(nextConfig);
+  }
+
+  return nextConfig;
 };
-
-export default withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-})(nextConfig);
