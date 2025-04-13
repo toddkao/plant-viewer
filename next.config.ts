c172aa19419fb88d22148f6f9042b61988e121c0
Todd Kao <todd.kakao@gmail.com>
2025-04-13 19:08:20 -0400

    fix build

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-require-imports */
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require("next/constants");
 
-import { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } from "next/constants";
-
-module.exports = async (phase: string) => {
+module.exports = async (phase: any): Promise<import("next").NextConfig> => {
   /** @type {import("next").NextConfig} */
-  const nextConfig: import("next").NextConfig = {
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-  };
+  const nextConfig = {};
 
   if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
     const withSerwist = (await import("@serwist/next")).default({
